9ff8ea26886ca5ec30f1351e7c8f846e11e5eaf7
ychadwick <ychadwick>
2022-09-07 10:39:13 +0200

    added inputsize config if missing in DB

--- src/Resources/public/assets/js/app.js
+++ src/Resources/public/assets/js/app.js
@@ -9,7 +9,64 @@ define([
             var formModel = new MyFormModel({id: formId, url: url});
             if (formId != null && formId != "undefined") {
                 formModel.fetch({success: function (model) {
-                        var collection = new MyFormSnippetsCollection(model.get('collection'));
+                        let formFieldCollection = model.get('collection');
+
+                        formFieldCollection.forEach((item) => {
+                            let fields = item.fields
+                            if(!fields.inputsize) {
+                                fields.inputsize = {
+                                    "label": "Input Size",
+                                    "type": "select",
+                                    "value": [
+                                        {
+                                            "value": "",
+                                            "label": "Default",
+                                            "css_config": "default",
+                                            "selected": true
+                                        },
+                                        {
+                                            "value": "col-md-2",
+                                            "label": "Mini",
+                                            "css_config": "xs",
+                                            "selected": false
+                                        },
+                                        {
+                                            "value": "col-md-4",
+                                            "label": "Small",
+                                            "css_config": "s",
+                                            "selected": false
+                                        },
+                                        {
+                                            "value": "col-md-6",
+                                            "label": "Medium",
+                                            "css_config": "m",
+                                            "selected": false
+                                        },
+                                        {
+                                            "value": "col-md-8",
+                                            "label": "Large",
+                                            "css_config": "l",
+                                            "selected": false
+                                        },
+                                        {
+                                            "value": "col-md-10",
+                                            "label": "Xlarge",
+                                            "css_config": "xl",
+                                            "selected": false
+                                        },
+                                        {
+                                            "value": "col-md-12",
+                                            "label": "Xxlarge",
+                                            "css_config": "xxl",
+                                            "selected": false
+                                        }
+                                    ]
+
+                                }
+                            }
+                        })
+
+                        var collection = new MyFormSnippetsCollection(formFieldCollection);
                         new MyFormView({
                             model: model, collection: collection, backToListUri: backToListUri
                         });
